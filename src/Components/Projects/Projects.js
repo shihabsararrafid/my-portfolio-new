687c1ea5f1f3fa6e39e34cf423b6d15891ccaaf2
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-06-11 18:05:01 +0600

    fixed some bug

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SingProject from "./SingProject";
 
-const Projects = ({ len }) => {
+const Projects = ({ len, open, setOpen }) => {
   const [project, setProject] = useState([]);
   useEffect(() => {
     fetch("projects.json")
@@ -11,7 +11,12 @@ const Projects = ({ len }) => {
   return (
     <div>
       <h1 className="text-5xl font-bold text-[#08fdd8] my-20 ">Projects</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+
+      <div
+        className={` ${
+          open ? "hidden" : "grid"
+        } grid-cols-1 lg:grid-cols-2 gap-2 `}
+      >
         {project.slice(0, len).map((p) => (
           <SingProject
             id={p.id}
