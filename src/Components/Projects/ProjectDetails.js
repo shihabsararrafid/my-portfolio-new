91610b1e23388c95fac282a05dc5ce6fa892188f
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-06-11 06:52:00 +0600

    projct detail btn added

--- src/Components/Projects/ProjectDetails.js
+++ src/Components/Projects/ProjectDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import ShowBar from "../Hooks/ShowBar";
 
 const ProjectDetails = ({ open, setOpen }) => {
@@ -19,10 +19,10 @@ const ProjectDetails = ({ open, setOpen }) => {
     <div className="bg-[#1D1D1D]  text-white px-10 w-full min-h-screen lg:w-[90%]">
       <ShowBar open={open} setOpen={setOpen}></ShowBar>
       <h1 className="text-5xl text-blue-500 my-7 font-bold">Project Details</h1>
-      <h1 className="text-5xl font-bold font-mono text-white text-center my-5">
-        {selectedItem.name}
+      <h1 className="text-5xl font-bold font-mono text-white mx-auto md:w-[80%] w-full lg:w-[50%] my-5">
+        {selectedItem?.name}
       </h1>
-      <div class="h-96 carousel mx-auto md:w-[80%] w-full lg:w-[50%] carousel-vertical rounded-box">
+      <div class="h-96 my-10 carousel mx-auto md:w-[80%] w-full lg:w-[50%] carousel-vertical rounded-box">
         <div class="carousel-item h-full">
           <img
             className="md:w-[80%] w-full lg:w-[50%] object-cover"
@@ -41,6 +41,33 @@ const ProjectDetails = ({ open, setOpen }) => {
             src={selectedItem?.shot3}
           />
         </div>
+      </div>
+      <div className="button flex lg:flex-row flex-col gap-y-4 justify-center mx-auto md:w-[80%] w-full lg:w-[50%] ">
+        <a
+          href={selectedItem?.liveSite}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="btn mx-5 btn-primary">Live Site</button>
+        </a>
+        <a
+          href={selectedItem?.clientSide}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="btn mx-5 btn-primary">Client Code</button>
+        </a>
+        {selectedItem?.serverSide == " " ? (
+          ""
+        ) : (
+          <a
+            href={selectedItem?.serverSide}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="btn mx-5 btn-primary">Server Code</button>
+          </a>
+        )}
       </div>
     </div>
   );
